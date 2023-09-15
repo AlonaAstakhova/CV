43d1e191db3036367c2f0c4529bd00b4fa404dc2
AlonaAstakhova <alyonasta@gmail.com>
2023-09-15 12:30:51 -0700

    My portfolio, page's and contacts' links, favicon

--- index.js
+++ index.js
@@ -24,3 +24,10 @@ document.body.addEventListener('click', (event) => {
   // Действие при клике
   document.querySelector('.header').classList.remove('open')
 })
+
+let links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
+    }
+}
