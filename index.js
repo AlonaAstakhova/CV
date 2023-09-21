a02c659a917f9265beb69ac191a548978de87375
AlonaAstakhova <alyonasta@gmail.com>
2023-09-20 18:53:34 -0700

    modal start

--- index.js
+++ index.js
@@ -25,9 +25,19 @@ document.body.addEventListener('click', (event) => {
   document.querySelector('.header').classList.remove('open')
 })
 
-let links = document.querySelectorAll(".menu-item > a");
+let links = document.querySelectorAll('.menu-item > a')
 for (let i = 0; i < links.length; i++) {
-    links[i].onclick = function () {
-        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
-    }
+  links[i].onclick = function () {
+    document
+      .getElementById(links[i].getAttribute('data-link'))
+      .scrollIntoView({ behavior: 'smooth' })
+  }
 }
+
+document.getElementById('send-message').addEventListener('click', function () {
+  document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById('close-modal').addEventListener('click', function () {
+  document.getElementById('modal').classList.remove('open')
+})
